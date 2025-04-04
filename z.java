20f002607c1ed52cb326a15a835058d6c92cacf0
pooja48-patil <poojabadappanavar89@gmail.com>
2025-04-04 08:28:18 +0530

    Update z.java

--- z.java
+++ z.java
@@ -1,7 +1,5 @@
 class first{
     public staic void main method(string()arg){
         system.out.println("WELCOME");
-        system.out.println("devops");
-        system.out.println("C DIVISION");
     }
 } 
